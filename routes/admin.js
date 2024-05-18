1318039c2937c7484d4ef40a70f4d8174652b8a2
Ferhat Sanli <ferhatsanli.sea@gmail.com>
2024-05-18 09:57:07 +0200

    I forgot to save admin and shop js files before commiting.

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,6 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('/users', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'users.html'));
-});
 
 // ***GET*** request: filling the user list here
 router.get('/users', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
 
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 
 });
 
